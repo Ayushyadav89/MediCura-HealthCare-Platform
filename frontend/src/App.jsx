d6320885beed3490c324cf1e75b24b988889266a
Ayushyadav89 <2k22.cse.2213307@gmail.com>
2025-07-03 23:03:20 +0530

    We are add new pages jsx file which contains all element

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import  { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home.jsx'
+import Doctor from './pages/Doctor.jsx'
+import Login from './pages/Login.jsx'
+import About from './pages/About.jsx'
+import Contact from './pages/Contact.jsx'
+import MyProfile from './pages/MyProfile.jsx'
+import MyAppointment from './pages/MyAppointment.jsx'
 
 function App() {
   return (
-    <div>
-      
+    <div className='mx-4 sm:mx-[10%]'>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/doctors' element={<Doctor />} />
+        <Route path='/doctor/:speciality' element={<Doctor />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/my-profile' element={<MyProfile />} />
+        <Route path='/my-appointments' element={<MyAppointment />} />
+      </Routes>
     </div>
   )
 }
